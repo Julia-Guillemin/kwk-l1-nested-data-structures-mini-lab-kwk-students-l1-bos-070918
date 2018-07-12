741fa0b3284d3281edbeabbe0174e02cf3043893
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-12 14:31:53 +0000

    automatically backed up by learn

--- moduelo.rb
+++ moduelo.rb
@@ -1,9 +1,9 @@
 chariter_list ={
-  "Monica" => "Neat Freek"
-  "Joey" => "Jokster"
-  "Chandler" => "Narcastic n00b"
-  "Pheebe" => "Free Spiret"
-  "Rachle" => "Devorcec"
+  "Monica" => "Neat Freek",
+  "Joey" => "Jokster",
+  "Chandler" => "Narcastic n00b",
+  "Pheebe" => "Free Spiret",
+  "Rachle" => "Devorcec",
 }
 
 chariter_list.each do |chariter_name, chariteristic|
