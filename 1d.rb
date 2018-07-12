1bbf496eb4a0418b0277c70a760d6f403f07357d
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-12 14:12:59 +0000

    automatically backed up by learn

--- 1d.rb
+++ 1d.rb
@@ -10,6 +10,9 @@ one_direction.each do |names_1D, ages_1D|
   puts "name #{names_1D} is ages: #{ages_1D} years old"
 end
 
+ages = 0
+
+
 # How can you print out all of the members and their ages like the following:
 # Zayn is 25 years old.
 # Liam is 24 years old.
